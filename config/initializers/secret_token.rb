c49c70d5d6aeb07dd99b3da35aea59a26e7f5b2e
rajat_imac <rajat@octathorpeweb.com>
2013-12-31 16:12:01 +0530

    initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CricketWorld::Application.config.secret_key_base = '06d53e50a2c4b1aeacdfbfa2d715ab75d4bbef2a709c2de9320afa2bba7fff747dac8fdf2dddad097995851d5322c527e7566e813a1a809991c148625085338f'
