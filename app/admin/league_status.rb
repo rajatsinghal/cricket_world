b43b68abd8f2d86d68596f1dc466a0ae75be7c60
rajat_imac <rajat@octathorpeweb.com>
2014-02-03 16:00:24 +0530

    league positions form

--- app/admin/league_status.rb
+++ app/admin/league_status.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register LeagueStatus, :as => "League Position" do
   
   menu false
-  actions :all, except: [:show]
+  actions :all, except: [:show, :new]
   config.sort_order = "points_desc"
   
   permit_params :league_id, :team_id, :matches_played, :position, :wins, :losses, :ties, :points, :run_rate
@@ -23,4 +23,20 @@ ActiveAdmin.register LeagueStatus, :as => "League Position" do
       column :run_rate
       actions
   end
+  
+  form do |f|
+    f.inputs do
+      f.input :league, :input_html => { :disabled => true } 
+      f.input :team, :input_html => { :disabled => true } 
+      f.input :matches_played
+      f.input :position
+      f.input :wins
+      f.input :losses
+      f.input :ties
+      f.input :points
+      f.input :run_rate
+    end
+    f.actions
+  end
+  
 end
