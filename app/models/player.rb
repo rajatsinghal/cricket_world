ef89c314106aced36ed8b5ec70bbf18b2d2c95d3
rajat_imac <rajat@octathorpeweb.com>
2014-02-03 17:11:42 +0530

    validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
   
+  validates_presence_of :team_id
+  
   scope :filter_team, lambda {|team_id| where("team_id = ?", team_id)}
   
   def name
--- app/models/team_manager.rb
+++ app/models/team_manager.rb
@@ -1,4 +1,6 @@
 class TeamManager < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
+  
+  validates_presence_of :team_id
 end
